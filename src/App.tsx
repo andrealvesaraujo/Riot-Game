801bb89ec27e855f2c500e701187d3d132bb2398
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-01-29 18:40:54 -0300

    feature: add images, fonts, icons, css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import {FiShare2, FiTwitter, FiFacebook, FiInstagram} from "react-icons/fi"
 
 function App() {
   return (
@@ -7,6 +8,10 @@ function App() {
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <FiShare2/>
+          <FiInstagram />
+          <FiTwitter />
+          <FiFacebook />
         </p>
         <a
           className="App-link"
