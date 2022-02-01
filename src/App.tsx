712edee0103f64973474620682229b9e4c9d5091
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-02-01 12:03:34 -0300

    feature: Add semantic HTML tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,54 @@
 import React from 'react';
 import './App.scss';
 import {FiShare2, FiTwitter, FiFacebook, FiInstagram} from "react-icons/fi"
-
+         
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          <FiShare2/>
-          <FiInstagram />
-          <FiTwitter />
-          <FiFacebook />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <div>
+          {/* Logo */}
+        </div>
+        <div>Riot Games</div>
+        <div><FiShare2/></div>
       </header>
-    </div>
+      <section>
+        <div>
+          <h1>Valorant</h1>
+          <div>
+            <div>
+              {/* Omem */}
+            </div>
+            <div>
+              {/* Sage */}
+            </div>
+            <div>
+              {/* Sova */}
+            </div>
+          </div>
+          <footer>
+            <div>
+              <a href="https://www.instagram.com/valorantbrasil/?hl=pt-br" target="_blank" rel="noreferrer"  >
+                <FiInstagram />
+              </a> 
+            </div>
+            <div>
+              <a href="https://twitter.com/playvalorant" target="_blank" rel="noreferrer"  >
+                <FiTwitter />
+              </a> 
+            </div>
+            <div>
+              <a href="https://pt-br.facebook.com/VALORANTbrasil/" target="_blank" rel="noreferrer"  >
+                <FiFacebook />
+              </a> 
+            </div>
+          </footer>
+        </div>
+        <div>
+          {/* Main Character */}
+        </div>
+      </section>
+    </main>
   );
 }
 
